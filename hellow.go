defa3b7d6ea12849542dcc108b09f42eafed58d8
“hossain01785” <hossain01785@gmail.com>
2023-03-05 20:38:43 +0600

    hellow.go

---
+++ hellow.go
@@ -0,0 +1,88 @@
+/*package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hellow WOrld")
+	fmt.Println("hellow WOrld")
+	fmt.Println("hellow WOrld")
+	fmt.Println("hellow WOrld")
+	fmt.Println("hellow WOrld")
+
+
+	fmt.Println("hellow WOrld")
+
+}*/
+
+/*package main
+
+//func main() { fmt.Println("Hello World!") }
+
+
+var a int = 10
+var:=20*/
+
+/*package main
+
+import "fmt"
+
+const PI = 3.1416
+
+func main() {
+	fmt.Println(PI)
+
+}*/
+
+/*package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var h string = "Hossain ALi"
+	var a int = 15
+
+	/*fmt.Printf("i has value: %v and type: %T\n", i, i)
+	fmt.Printf("j has value: %v and type: %T", j, j)*/
+
+/*fmt.Printf("MY name is: %v and type: %T\n", h, a)
+	fmt.Printf("j has value: %v and type: %T", h, a)
+}*/
+
+/*package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = 15.5
+	var txt = "Hello World!"
+
+	fmt.Printf("%v", a)
+	fmt.Printf("%#v\n", a)
+	fmt.Printf("%v%%\n", a)
+	fmt.Printf("%T\n", a)
+
+	fmt.Printf("%v\n", txt)
+	fmt.Printf("%#v\n", txt)
+	fmt.Printf("%T\n", txt)
+}*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var a bool = true
+	var b int = 4
+	var c float32 = 1.23
+	var d string = "hyi"
+
+	fmt.Println("boolean: ", a)
+	fmt.Println("int:", b)
+	fmt.Println("float", c)
+	fmt.Println("string: ", d)
+}
