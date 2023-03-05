0a6c5e6fe1e4c0835626df407d0e6776d8be03c5
“hossain01785” <hossain01785@gmail.com>
2023-03-05 20:45:22 +0600

    switch.go

---
+++ switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	day := 4
+	switch day {
+	case 1:
+		fmt.Println("This Is Sonibar")
+	case 2:
+		fmt.Println("THis IS robibar")
+	default:
+		fmt.Println("This Is Weakend")
+	}
+
+}
